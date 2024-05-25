e307acaae703b5a842eb6cf491c5e69996274921
billkenney <30010560+billkenney@users.noreply.github.com>
2024-05-25 13:44:33 -0500

    Update revert.sh

--- revert.sh
+++ revert.sh
@@ -6,14 +6,12 @@ please enter the password 'makerbase' if prompted for a password (unless you've 
 sudo sed -i ‘s/deb.debian.org\/debian buster-backports/archive.debian.org\/debian buster-backports/g’ /etc/apt/sources.list
 cd /home/mks
 sudo rm -rf fluidd klipper klippy-env moonraker moonraker-env
-echo "cloning qidi's klipper repo..."
-git clone https://github.com/QIDITECH/klipper.git
-echo "cloning qidi's moonraker repo..."
-git clone https://github.com/QIDITECH/moonraker.git
+echo "downloading qidi's klipper repo..."
+wget --no-check-certificate 'https://codeload.github.com/QIDITECH/klipper/zip/refs/heads/main' -O klipper.zip ; unzip klipper.zip ; rm klipper.zip
+echo "downloading qidi's moonraker repo..."
+wget --no-check-certificate 'https://codeload.github.com/QIDITECH/moonraker/zip/refs/heads/main' -O moonraker.zip ; unzip moonraker.zip ; rm moonraker.zip
 echo "downloading fluidd 1.28..."
-wget 'https://docs.google.com/uc?export=download&id=1Essar3y789UNelY1G5T_Dro7V41O4m4K' -O fluidd-1.28.tgz
-tar -xzf fluidd-1.28.tgz
-rm fluidd-1.28.tgz
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1Essar3y789UNelY1G5T_Dro7V41O4m4K' -O fluidd-1.28.tgz ; tar -xzf fluidd-1.28.tgz ; rm fluidd-1.28.tgz
 echo "reinstalling klipper..."
 chmod +x ~/klipper/scripts/install-debian.sh
 sh ~/klipper/scripts/install-debian.sh
