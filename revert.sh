1a66551c45ee064737233a43fca227e13933d97a
billkenney <30010560+billkenney@users.noreply.github.com>
2024-05-27 16:17:31 -0500

    Update revert.sh

--- revert.sh
+++ revert.sh
@@ -10,9 +10,10 @@ cd /home/mks
 sudo rm -rf fluidd klipper klippy-env moonraker moonraker-env
 echo "downloading qidi's klipper repo..."
 wget --no-check-certificate https://raw.githubusercontent.com/billkenney/revert_qidi_software/main/klipper.tgz ; tar -xzf klipper.tgz ; rm klipper.tgz
+wget --no-check-certificate https://raw.githubusercontent.com/billkenney/revert_qidi_software/main/klipper-lib.tgz ; tar -xzf klipper-lib.tgz ; rm klipper-lib.tgz
 echo "downloading qidi's moonraker repo..."
 wget --no-check-certificate https://raw.githubusercontent.com/billkenney/revert_qidi_software/main/moonraker.tgz ; tar -xzf moonraker.tgz ; rm moonraker.tgz
-echo "downloading fluidd 1.28..."
+echo "downloading and installing fluidd 1.28..."
 wget --no-check-certificate https://raw.githubusercontent.com/billkenney/revert_qidi_software/main/fluidd-1.28.tgz ; tar -xzf fluidd-1.28.tgz ; rm fluidd-1.28.tgz
 echo "reinstalling klipper..."
 chmod +x ~/klipper/scripts/install-debian.sh
