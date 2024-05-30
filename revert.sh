3edceb3de7befea61cd46f9ef58374b14fc4a2eb
billkenney <30010560+billkenney@users.noreply.github.com>
2024-05-30 11:08:19 -0500

    Update revert.sh

--- revert.sh
+++ revert.sh
@@ -7,19 +7,13 @@ sudo sed -i ‘s/deb.debian.org\/debian buster-backports/archive.debian.org\/deb
 sudo mv ~/klipper_config/MKS_THR.cfg ~/klipper_config/MKS_THR.cfg.bak ; path=$(ls /dev/serial/by-id/*) ; printf "[mcu MKS_THR]\nserial:$path\n" > ~/klipper_config/MKS_THR.cfg
 sudo service klipper stop ; sudo service moonraker stop
 cd /home/mks
-sudo rm -rf fluidd klipper klippy-env moonraker moonraker-env
+sudo rm -rf fluidd klipper moonraker
 echo "downloading qidi's klipper repo..."
 wget --no-check-certificate https://raw.githubusercontent.com/billkenney/revert_qidi_software/main/klipper.tgz ; tar -xzf klipper.tgz ; rm klipper.tgz
 wget --no-check-certificate https://raw.githubusercontent.com/billkenney/revert_qidi_software/main/klipper-lib.tgz ; tar -xzf klipper-lib.tgz ; rm klipper-lib.tgz
 echo "downloading qidi's moonraker repo..."
 wget --no-check-certificate https://raw.githubusercontent.com/billkenney/revert_qidi_software/main/moonraker.tgz ; tar -xzf moonraker.tgz ; rm moonraker.tgz
-echo "downloading and installing fluidd 1.28..."
+echo "downloading fluidd 1.28..."
 wget --no-check-certificate https://raw.githubusercontent.com/billkenney/revert_qidi_software/main/fluidd-1.28.tgz ; tar -xzf fluidd-1.28.tgz ; rm fluidd-1.28.tgz
-echo "reinstalling klipper..."
-chmod +x ~/klipper/scripts/install-debian.sh
-sh ~/klipper/scripts/install-debian.sh
-echo "reinstalling moonraker..."
-chmod +x ~/moonraker/scripts/install-moonraker.sh
-sh ~/moonraker/scripts/install-moonraker.sh
 echo "klipper and moonraker have been installed from qidi's github repository, and fluidd has been updated to the latest version that works with qidi's modified software"
 echo "turn your printer off and on and you should be good to go"
