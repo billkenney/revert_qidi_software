21a329d4fd032b4c58b361fce175ec2a13e7f661
billkenney <30010560+billkenney@users.noreply.github.com>
2024-08-23 14:32:22 -0500

    Update revert.sh

--- revert.sh
+++ revert.sh
@@ -15,5 +15,7 @@ echo "downloading qidi's moonraker repo..."
 wget --no-check-certificate https://raw.githubusercontent.com/billkenney/revert_qidi_software/main/moonraker.tgz ; tar -xzf moonraker.tgz ; rm moonraker.tgz
 echo "downloading fluidd 1.28..."
 wget --no-check-certificate https://raw.githubusercontent.com/billkenney/revert_qidi_software/main/fluidd-1.28.tgz ; tar -xzf fluidd-1.28.tgz ; rm fluidd-1.28.tgz
+sudo chown -R mks:mks /home/mks/klipp* /home/mks/moonraker* /home/mks/fluidd
+
 echo "klipper and moonraker have been installed from qidi's github repository, and fluidd has been updated to the latest version that works with qidi's modified software"
 echo "turn your printer off and on and you should be good to go"
